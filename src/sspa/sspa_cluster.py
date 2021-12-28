d47b88783999588f0bbe57e1f23a43690b3e4243
cwieder <cwieder09@gmail.com>
2021-12-28 12:31:40 +0000

    add cluster method

---
+++ src/sspa/sspa_cluster.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+
+def sspa_cluster(mat, pathways, projection=False):
+    pathway_matrices = []
+    pathway_ids = []
+    for pathway, compounds in pathways.items():
+        single_pathway_matrix = mat.drop(mat.columns.difference(compounds), axis=1)
+        if single_pathway_matrix.shape[1] >= 1:
+            pathway_matrices.append(single_pathway_matrix.values)
+            pathway_ids.append(pathway)
+
+    if projection:
+        scores = []
+        for m in pathway_matrices:
+            kmeans = KMeans(n_clusters=2).fit(m)
+            centroids1 = kmeans.cluster_centers_[0]
+            centroids2 = kmeans.cluster_centers_[1]
+
+            vec = centroids1 - centroids2
+            unit_vec = vec / np.linalg.norm(vec)
+            proj_data = unit_vec.dot(m.T)
+            scores.append(proj_data)
+
+    else:
+        scores = []
+        for m in pathway_matrices:
+            kmeans = KMeans(n_clusters=2)
+            new_data = kmeans.fit_transform(m)
+            scores.append(new_data[:, 0])
+
+    scores_df = pd.DataFrame(scores, columns=mat.index, index=pathways.keys())
+    return scores_df
