b202ee895abf7665328d0fb545cb43ce546442da
cwieder <cwieder09@gmail.com>
2022-01-04 11:51:11 +0000

    add docstring utils.py

--- src/sspa/utils.py
+++ src/sspa/utils.py
@@ -5,7 +5,9 @@ def load_example_data(omicstype="metabolomics"):
     """
     Loads example datasets
     :param omicstype: type of omics for example data. Available options are "metabolomics" or "transcriptomics"
+    Metabolomics data are from Su et al 2020 https://doi.org/10.1016/j.cell.2020.10.037
     :return: pre-processed omics data matrix consisting of m samples and n entities (metabolites/genes)
+    in the form of a pandas DataFrame. Contains one of more metadata columns at the end.
     """
 
     if omicstype == "metabolomics":
