3af8cfef630f08f090cf13b1a2fc83918529ebd4
cwieder <cwieder09@gmail.com>
2022-02-03 12:34:43 +0000

    fix matrix orientation

--- src/sspa/sspa_zscore.py
+++ src/sspa/sspa_zscore.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 
-def sspa_zscore(mat, pathways):
+def sspa_zscore(mat, pathways, min_entity=2):
     """
     Lee at al 2008 z-score method for single sample pathway analysis
 
@@ -14,18 +14,19 @@ def sspa_zscore(mat, pathways):
 
     :return: pandas DataFrame of pathway scores derived using the z-score method. Columns represent pathways and rows represnt samples.
     """
-
-    mat_t = mat.T
     pathway_activities = []
+    pathway_ids = []
+    for pathway, compounds in pathways.items():
+        single_pathway_matrix = mat.drop(mat.columns.difference(compounds), axis=1)
+        if single_pathway_matrix.shape[1] >= min_entity:
+            pathway_mat = single_pathway_matrix.T.values
+            pathway_ids.append(pathway)
 
-    for k, v in pathways.items():
-        pathway_mat = mat_t.iloc[mat_t.index.isin(v), :]
-        pathway_mat = pathway_mat.to_numpy(dtype=float)
-        zscores = stats.zscore(pathway_mat, axis=1)
-        sum_zscore = np.sum(zscores, axis=0)
-        pathway_act = sum_zscore / np.sqrt(pathway_mat.shape[0])
-        pathway_activities.append(pathway_act)
+            zscores = stats.zscore(pathway_mat, axis=1)
+            sum_zscore = np.sum(zscores, axis=0)
+            pathway_act = sum_zscore / np.sqrt(pathway_mat.shape[0])
+            pathway_activities.append(pathway_act)
 
-    pathway_activities_df = pd.DataFrame(pathway_activities, columns=mat.index, index=pathways.keys())
+    pathway_activities_df = pd.DataFrame(pathway_activities, columns=mat.index, index=pathway_ids).T
     return pathway_activities_df
 
