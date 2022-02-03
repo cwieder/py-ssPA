66adb91931ebd110125e7a93f810370ebe89fa88
cwieder <cwieder09@gmail.com>
2022-02-03 12:14:27 +0000

    fix matrix orientation

--- src/sspa/sspa_svd.py
+++ src/sspa/sspa_svd.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def sspa_svd(mat, paths):
+def sspa_svd(mat, pathways, min_entity=2):
 
     """
     Tomfohr et al 2007 SVD/PLAGE method for single sample pathway analysis
@@ -16,20 +16,26 @@ def sspa_svd(mat, paths):
 
     # Transpose the matrix for SVD
 
-    mat_t = mat.T
+    # mat_t = mat.T
     pathway_activities = []
 
     # Create pathway matrices
-    for k, v in paths.items():
-        pathway_mat = mat_t.iloc[mat_t.index.isin(v), :]
-        pathway_mat = pathway_mat.to_numpy(dtype=float)
+    # pathway_matrices = []
+    pathway_ids = []
+    for pathway, compounds in pathways.items():
+        single_pathway_matrix = mat.drop(mat.columns.difference(compounds), axis=1)
+        if single_pathway_matrix.shape[1] >= min_entity:
+            # pathway_matrices.append(single_pathway_matrix.values)
+            pathway_ids.append(pathway)
+            pathway_mat = single_pathway_matrix.values
+            pathway_mat = pathway_mat.to_numpy(dtype=float)
 
-        # s = singular values
-        # u = left singular vector
-        # v = right singular vector
-        u, s, vh = np.linalg.svd(pathway_mat)
+            # s = singular values
+            # u = left singular vector
+            # v = right singular vector
+            u, s, vh = np.linalg.svd(pathway_mat)
 
-        pathway_activities.append(vh[0])
+            pathway_activities.append(vh[0])
 
-    pathway_activities_df = pd.DataFrame(pathway_activities, columns=mat.index, index=paths.keys())
+    pathway_activities_df = pd.DataFrame(pathway_activities, columns=mat.index, index=pathway_ids).T
     return pathway_activities_df
