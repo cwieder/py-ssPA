1365dfaf48e005e3316f633ed4f4f5210d44c034
cwieder <cwieder09@gmail.com>
2022-02-14 16:10:44 +0000

    refactor pathway input using GMT style

--- src/sspa/process_pathways.py
+++ src/sspa/process_pathways.py
@@ -11,52 +11,56 @@ class ProcessPathways:
      the ChEBI2Reactome_All_levels file from https://reactome.org/download-data and specify the full filepath to this.
     :param organism : the organism requried when using Reactome. Full name must be specified as in Reacome db e.g. ('Homo sapiens' or 'Danio rerio')
 
-    :returns pathway entity dictionary (composed of pathway IDs and the entities each pathway is composed of),
-     and pathway name mapping dict (composed of pathway ID: pathway name)
+    :returns pathway DataFrame in GMT format. Each row represents a pathway. First column is ID, second is full name, rest of columns represent entities using DB specific identifiers. 
     """
 
-    def __init__(self, infile, organism):
+    def __init__(self, infile=None):
         self.infile = infile
-        self.organism = organism
 
-    def process_reactome(self):
+    def process_reactome(self, organism):
         # Process CHEBI to reactome data
 
-        if self.infile == "R78":
+        if self.infile == None or self.infile == "R78":
             stream = pkg_resources.resource_stream(__name__, 'pathway_databases/ChEBI2Reactome_All_Levels_R78.txt')
             f = pd.read_csv(stream, sep="\t", header=None, encoding='latin-1')
         else:
             f = pd.read_csv(self.infile, sep="\t", header=None)
         f.columns = ['CHEBI', 'pathway_ID', 'link', 'pathway_name', 'evidence_code', 'species']
-        f_filt = f[f.species == self.organism]
+        f_filt = f[f.species == organism]
         name_dict = dict(zip(f_filt['pathway_ID'], f_filt['pathway_name']))
 
         groups = f_filt.groupby(['pathway_ID'])['CHEBI'].apply(list).to_dict()
         df = pd.DataFrame.from_dict(groups, orient='index', dtype="object")
-
         pathways_df = df.dropna(axis=0, how='all', subset=df.columns.tolist()[1:])
-        pathways = pathways_df.index.tolist()
-        pathway_dict = {}
+        pathways_df = df.dropna(axis=1, how='all')
 
-        for pathway in pathways:
-            pathway_compounds = list(set(pathways_df.loc[pathway, :].tolist()))
-            pathway_compounds = [str(i) for i in pathway_compounds if str(i) != "None"]
+        # Remove duplicated compounds
+        mask = pathways_df.apply(pd.Series.duplicated, 1) & pathways_df.astype(bool)
+        pathways_df = pathways_df.mask(mask, None)
+        pathways_df["Pathway_name"] = pathways_df.index.map(name_dict)
+        pathways_df.insert(0, 'Pathway_name', pathways_df.pop('Pathway_name'))
 
-            cpds = pathway_compounds[1:]
-            if len(cpds) > 1:
-                pathway_dict[pathway] = cpds
-        return pathway_dict, name_dict
+        return pathways_df
 
     def process_kegg(self):
-        if self.infile == "R98":
+        if self.infile == None or self.infile == "R98":
             stream = pkg_resources.resource_stream(__name__, 'pathway_databases/KEGG_human_pathways_compounds_R98.csv')
-            f = pd.read_csv(stream, index_col=0, encoding='latin-1')
+            pathways_df = pd.read_csv(stream, index_col=0, encoding='latin-1')
         else:
-            f = pd.read_csv(self.infile, index_col=0)
-        name_dict = dict(zip(f.index, f['Pathway_name']))
-        pathway_dict = {k: list(set(f.loc[k, '0':].tolist())) for k in list(name_dict.keys())}
-        pathway_dict = {k: [i for i in v if pd.notnull(i)] for k, v in pathway_dict.items()}
-        pathway_dict = {k: v for k, v in pathway_dict.items() if len(v) > 2}
+            pathways_df = pd.read_csv(self.infile, index_col=0)
 
-        return pathway_dict, name_dict
-        # remove dupes
+        pathways_df = pathways_df.dropna(axis=0, how='all', subset=pathways_df.columns.tolist()[1:])
+        pathways_df = pathways_df.dropna(axis=1, how='all')
+
+        # Remove duplicated compounds
+        mask = pathways_df.apply(pd.Series.duplicated, 1) & pathways_df.astype(bool)
+        pathways_df = pathways_df.mask(mask, None)
+
+        return pathways_df
+
+    def process_gmt(self):
+        pathways_df = pd.read_csv(self.infile, index_col=0)
+        pathways_df = pathways_df.dropna(axis=0, how='all', subset=pathways_df.columns.tolist()[1:])
+        pathways_df = pathways_df.dropna(axis=1, how='all')
+
+        return pathways_df
