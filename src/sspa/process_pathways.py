b4ab0499c67b109b67e9e883739a61e09dc31770
cwieder <cwieder09@gmail.com>
2022-02-18 10:46:54 +0000

    update processpathways

--- src/sspa/process_pathways.py
+++ src/sspa/process_pathways.py
@@ -1,34 +1,39 @@
 import pandas as pd
 import pkg_resources
-import download_pathways
+import sspa.download_pathways 
 
-def process_reactome(organism, infile=None, download_latest=False):
+def process_reactome(organism, infile=None, download_latest=False, filepath=None):
     # Process CHEBI to reactome data
 
-    if infile == None or infile == "R78":
-        stream = pkg_resources.resource_stream(__name__, 'pathway_databases/ChEBI2Reactome_All_Levels_R78.txt')
-        f = pd.read_csv(stream, sep="\t", header=None, encoding='latin-1')
+    if download_latest:
+        pathways_df = sspa.download_pathways.download_reactome(organism, filepath)
+        return pathways_df
+        
     else:
-        f = pd.read_csv(infile, sep="\t", header=None)
-    f.columns = ['CHEBI', 'pathway_ID', 'link', 'pathway_name', 'evidence_code', 'species']
-    f_filt = f[f.species == organism]
-    name_dict = dict(zip(f_filt['pathway_ID'], f_filt['pathway_name']))
+        if infile == None or infile == "R78":
+            stream = pkg_resources.resource_stream(__name__, 'pathway_databases/ChEBI2Reactome_All_Levels_R78.txt')
+            f = pd.read_csv(stream, sep="\t", header=None, encoding='latin-1')
+        else:
+            f = pd.read_csv(infile, sep="\t", header=None)
+        f.columns = ['CHEBI', 'pathway_ID', 'link', 'pathway_name', 'evidence_code', 'species']
+        f_filt = f[f.species == organism]
+        name_dict = dict(zip(f_filt['pathway_ID'], f_filt['pathway_name']))
 
-    groups = f_filt.groupby(['pathway_ID'])['CHEBI'].apply(list).to_dict()
-    groups = {k: list(set(v)) for k, v in groups.items()}
-    df = pd.DataFrame.from_dict(groups, orient='index', dtype="object")
-    pathways_df = df.dropna(axis=0, how='all', subset=df.columns.tolist()[1:])
-    pathways_df = df.dropna(axis=1, how='all')
+        groups = f_filt.groupby(['pathway_ID'])['CHEBI'].apply(list).to_dict()
+        groups = {k: list(set(v)) for k, v in groups.items()}
+        df = pd.DataFrame.from_dict(groups, orient='index', dtype="object")
+        pathways_df = df.dropna(axis=0, how='all', subset=df.columns.tolist()[1:])
+        pathways_df = df.dropna(axis=1, how='all')
 
-    pathways_df["Pathway_name"] = pathways_df.index.map(name_dict)
-    pathways_df.insert(0, 'Pathway_name', pathways_df.pop('Pathway_name'))
+        pathways_df["Pathway_name"] = pathways_df.index.map(name_dict)
+        pathways_df.insert(0, 'Pathway_name', pathways_df.pop('Pathway_name'))
 
-    return pathways_df
+        return pathways_df
 
 def process_kegg(self, organism, infile=None, download_latest=False, filepath=None):
 
     if download_latest:
-        pathways_df = download_pathways(organism, filepath)
+        pathways_df = sspa.download_pathways.download_KEGG(organism, filepath)
         return pathways_df
 
     else:
