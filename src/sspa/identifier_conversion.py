919d7d5593a35656f0f452e4de357465605d6e0a
cwieder <cwieder09@gmail.com>
2022-02-24 17:26:08 +0000

    update id conversion

--- src/sspa/identifier_conversion.py
+++ src/sspa/identifier_conversion.py
@@ -4,20 +4,25 @@ import pandas as pd
 def identifier_conversion(input_type, compound_list):
     url = "http://api.xialab.ca/mapcompounds"
 
-    cpd_list = ";".join(compound_list)+";"
+    resps = []
+    for i in range(0, len(compound_list), 100):
+        cpds = compound_list[i:i+100]
+        input_cpds = {"queryList": cpds, "inputType": input_type}
+        headers = {
+            # 'Content-Type': "application/json",
+            'cache-control': "no-cache",
+            }
 
-    input_cpds = {"queryList": cpd_list, "inputType": input_type}
-    headers = {
-        'Content-Type': "application/json",
-        'cache-control': "no-cache",
-        }
+        response = requests.request("POST", url, json=input_cpds, headers=headers)
+        resp_dict = response.json()
+        resps.append(resp_dict)
 
-    response = requests.request("POST", url, json=input_cpds, headers=headers)
+    res_dict = {k: [d[k] for d in resps] for k in resps[0]}
+    res_dict = {k: [item for sublist in v for item in sublist] for k, v in res_dict.items()}
 
-    resp_dict = response.json()
-    resp_df = pd.DataFrame(resp_dict)
+    resp_df = pd.DataFrame(res_dict)
     return resp_df
-
+    
 def map_identifiers(query_df, output_id_type, matrix):
     # output id type can be any of ['HMDB', 'PubChem', 'ChEBI', 'KEGG', 'METLIN','SMILES']
     cpd_mapping_dict = dict(zip(query_df["Query"].tolist(), query_df[output_id_type].tolist()))
