1af4100be92ef257b7677679d96a4d5853f3c8eb
cwieder <cwieder09@gmail.com>
2022-02-25 10:26:02 +0000

    finish id conversion

--- src/sspa/identifier_conversion.py
+++ src/sspa/identifier_conversion.py
@@ -22,11 +22,11 @@ def identifier_conversion(input_type, compound_list):
 
     resp_df = pd.DataFrame(res_dict)
     return resp_df
-    
+
 def map_identifiers(query_df, output_id_type, matrix):
     # output id type can be any of ['HMDB', 'PubChem', 'ChEBI', 'KEGG', 'METLIN','SMILES']
     cpd_mapping_dict = dict(zip(query_df["Query"].tolist(), query_df[output_id_type].tolist()))
-    cpd_mapping_dict = {k: v for k, v in cpd_mapping_dict.items() if pd.notnull(v)}
+    cpd_mapping_dict = {k: v for k, v in cpd_mapping_dict.items() if v not in ["NA", np.nan, None, "None"]}
     renamed_mat = matrix.drop([i for i in matrix.columns if i not in cpd_mapping_dict.keys()], axis=1)
     renamed_mat = renamed_mat.rename(cpd_mapping_dict, axis=1)
     return renamed_mat
