2153cb6c0f394d50d085f3a129543e69ec287d41
cwieder <cwieder09@gmail.com>
2023-02-21 10:53:26 +0000

    update minor version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='sspa',
-    version='0.2.1',
+    version='0.2.2',
     packages=['sspa'],
     package_dir={'':'src'},
     url='https://github.com/cwieder/sspa',
