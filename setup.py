3dca6e40acb978a8908d2b4b61b92b3620430708
cwieder <cw2019@ic.ac.uk>
2024-01-15 19:04:29 +0000

    update minor release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='sspa',
-    version='1.0.2',
+    version='1.0.3',
     packages=['sspa'],
     package_dir={'':'src'},
     url='https://github.com/cwieder/sspa',
